2649844b47318db5ba746f3ce4cfc3f2a1669bf7
Pablo Fernandez <pablos.fernandezc@gmail.com>
2018-11-22 16:29:32 -0300

    views updated

--- resources/views/profile/field.blade.php
+++ resources/views/profile/field.blade.php
@@ -3,29 +3,24 @@
 
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <!-- profile image -->
-                <h3>Hola,</h3>
-                <!-- information -->
-                <img class="centered" src="">
-                <p>Cancha: </p>
-                <br>
-                <p>Ubicacion:</p> 
-                <br>
-                <p>Informacion de contacto:</p>
-                <br>
-                <p>Rating:</p>
-                <br>
-                <a class="button">Editar</a>
-                <!-- ADMIN -->
-                <p>Eliminar</p>
-                <br>
-            </div>
-        </div>
-    </div>
+<br>
+<div class="offset-1">
+    <h2>{{ $field->fieldName }}</h2>
+    <h4>Direccion {{ $field->address }}</h4>
+    <h4>Rating: {{ $field->rating }}</h4>
+    <h4>Barrio: {{ $field->zone }}</h4>
+    <h4>Telefono: {{ $field->contact}}</h4>
+    <h4>Email: {{ $field->email}}</h4>
+    <a class='btn' href='#'>Hacer reserva:</a>
+    <br>
+    <!-- ADMIN / FIELD USER -->
+    <a class="btn" href="#">Editar</a>
+    <br>
+    <!-- ADMIN -->
+    <a class="btn" href="#">Eliminar</a>
+    <br>
+
+    <a href="#">Volver</a>
 </div>
 @endsection
 
