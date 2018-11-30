6ba1b5ed35fcd938c37125c119f40b4b91416f41
lauticouget <lauticouget@gmail.com>
2018-11-30 13:06:49 -0300

    match migration/model = Guests/date complete

--- app/Match.php
+++ app/Match.php
@@ -14,10 +14,7 @@ class Match extends Model
         $this->hasMany(Team::class);
     }
     
-    public function guests()
-    {
-        return $this->hasOne(Guests::class);
-    }
+    
 
     public function team1()
     {
@@ -36,4 +33,55 @@ class Match extends Model
         $field = Field::find($this->field_id);
         return $field;
     }
+
+    
+   
+
+    // GUESTS -------------------------------
+        public function guest1()
+        {
+            $guest1 = User::find($this->guest1_id);
+            return $guest1;
+        }
+        
+        public function guest2()
+        {
+            $guest2 = User::find($this->guest2_id);
+            return $guest2;
+        }
+        public function guest3()
+        {
+            $guest3 = User::find($this->guest3_id);
+            return $guest3;
+        }
+        public function guest4()
+        {
+            $guest4 = User::find($this->guest4_id);
+            return $guest4;
+        }
+        public function guest5()
+        {
+            $guest5 = User::find($this->guest5_id);
+            return $guest5;
+        }
+        public function guest6()
+        {
+            $guest6 = User::find($this->guest6_id);
+            return $guest6;
+        }
+        public function guest7()
+        {
+            $guest7 = User::find($this->guest7_id);
+            return $guest7;
+        }
+        public function guest8()
+        {
+            $guest8 = User::find($this->guest8_id);
+            return $guest8;
+        }
+        public function guest9()
+        {
+            $guest9 = User::find($this->guest9_id);
+            return $guest9;
+        }
 }
--- database/migrations/2018_11_15_034545_table_matchs.php
+++ database/migrations/2018_11_15_034545_table_matchs.php
@@ -18,6 +18,16 @@ class TableMatchs extends Migration
             $table->foreign('team2_id')->references('id')->on('teams')->change();
             $table->foreign('score1_id')->references('id')->on('scores')->change();
             $table->foreign('score2_id')->references('id')->on('scores')->change();
+
+            $table->foreign('guest1')->references('id')->on('users');
+            $table->foreign('guest2')->references('id')->on('users');
+            $table->foreign('guest3')->references('id')->on('users');
+            $table->foreign('guest4')->references('id')->on('users');
+            $table->foreign('guest5')->references('id')->on('users');
+            $table->foreign('guest6')->references('id')->on('users');
+            $table->foreign('guest7')->references('id')->on('users');
+            $table->foreign('guest8')->references('id')->on('users');
+            $table->foreign('guest9')->references('id')->on('users');
         });
     }
 
